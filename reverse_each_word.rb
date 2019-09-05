219c6c02b8b662d322ac7490723eb6bcddaedd97
comic-engineer-3780 <ethanjosephfournier@gmail.com>
2019-09-05 21:32:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   words = string.split(" ")
-  reversed_words = collect.words{|i| i.reverse
-  words
+  reversed_words = collect.words{|i| i.reverse}
+  reversed_words
 end
 
 reverse_each_word("Hello my dear")
